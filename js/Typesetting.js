4ad6bbb1b567d255f6591b172b2ab4360cb82a2e
dongxincong <1414423031@qq.com>
2017-08-05 21:14:34 +0800

    实现匹配到句号就会换行的功能

--- js/Typesetting.js
+++ js/Typesetting.js
@@ -1,11 +1,10 @@
  /*
     @自动排版
  */
- 
- $("btn").onclick = function(){
+ $("btn").onclick = function() {
 
     search.style.textIndent = "22px";
     search.style.lineHeight = "1.8";
-    getReplace(/。/g,"。<br/><br/>&nbsp;&nbsp");
+    getReplace(/。/g, "。<br/><br/>&nbsp;&nbsp");
 
 }
