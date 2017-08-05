0d6001317e5291fd509ddc77d7d34f8ed9e1dcbc
dongxincong <1414423031@qq.com>
2017-08-05 21:11:43 +0800

    实现查找功能:如如输入张飞则快速匹配到含有张飞的词并高亮显示

--- js/search.js
+++ js/search.js
@@ -2,16 +2,16 @@
 /*
    @查找功能
 */
- $("findBtn").onclick = function(){
-         oCz = prompt("请输人查找的词语");
-         reg = new RegExp(oCz,"g");
-         searchs = $("search_text");
+ $("findBtn").onclick = function() {
+     oCz = prompt("请输人查找的词语");
+     reg = new RegExp(oCz, "g");
+     searchs = $("search_text");
 
-         search.innerHTML = searchs.value.replace(reg,"<span>"+oCz+"</span>");
-         //统计高亮的数量
-         var lens = searchs.value.match(reg).length;
+     search.innerHTML = searchs.value.replace(reg, "<span>" + oCz + "</span>");
+     //统计高亮的数量
+     var lens = searchs.value.match(reg).length;
 
-         $("len").innerHTML = "<i class='Reminder_icon'></i>温馨提示个数为:"+lens;
-         $("len").style.display = "block";
+     $("len").innerHTML = "<i class='Reminder_icon'></i>温馨提示个数为:" + lens;
+     $("len").style.display = "block";
 
-     }
+ }
