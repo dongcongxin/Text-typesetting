54ae16461f111428cd462f170766198c061d1061
dongxincong <1414423031@qq.com>
2017-08-07 17:02:56 +0800

    封装函数

--- js/getReplace.js
+++ js/getReplace.js
@@ -1,11 +1,8 @@
-    function getReplace(plaName,plaValue){
-    	
+    function getReplace(plaName, plaValue) {
+
         var search = document.getElementById("layer");
         var searchs = document.getElementById("search_text");
-        search.innerHTML=searchs.value.replace(plaName,plaValue);
-  
+        search.innerHTML = searchs.value.replace(plaName, plaValue);
+
     }
-
-
-
 
