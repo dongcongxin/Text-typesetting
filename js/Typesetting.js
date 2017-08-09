ccaf5e88292a9b781ee3999f03a3841341f6d8e5
dongxincong <1414423031@qq.com>
2017-08-09 20:00:54 +0800

    设置首行空格

--- js/Typesetting.js
+++ js/Typesetting.js
@@ -2,9 +2,9 @@
     @自动排版
  */
  $("btn").onclick = function() {
-
+    search.innerHTML =  $("search_text").value;
     search.style.textIndent = "22px";
     search.style.lineHeight = "1.8";
-    getReplace(/。/g, "。<br/><br/>&nbsp;&nbsp");
+    //getReplace(/。/g, "。<br/><br/>&nbsp;&nbsp");
 
 }
