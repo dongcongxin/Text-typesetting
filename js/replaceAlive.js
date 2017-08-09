cc8bfde1959b3418cde6a85aaeb4ad6474ba3da6
dongxincong <1414423031@qq.com>
2017-08-09 17:52:03 +0800

    加入一组敏感词

--- js/replaceAlive.js
+++ js/replaceAlive.js
@@ -1,6 +1,8 @@
-/*
+﻿/*
    @替换敏感词
 */
-$("replace").onclick = function() {
-    getReplace(/自杀/g, "**");
+
+$("replace").onclick = function() { 
+  getReplace(/自杀|周恩来|江泽民|跑路|骗子|诈骗|圈钱|保本|坏帐|保息/g,'**');
+   
 }
