0c03ce41faacb762f52fe9a8ed213b5ee95dfb74
dongxincong <1414423031@qq.com>
2017-08-20 18:50:59 +0800

    重构代码

--- js/main.js
+++ js/main.js
@@ -6,44 +6,63 @@
         @增加样式
         @增加自动加伪原创功能
    */
-    var $ = function(id) {
+   var $ = function(id) {
        return document.getElementById(id);
    }
    var search = $("layer");
 
    function _main() {
-
-       $("search_text").addEventListener('keyup', function() {
-           var math_length = 0;
-           math_length = getLenght(this.value);
-           if ($("search_text").value !== "") {
+     
+     function addListener(target, type, handler){
+     	
+     	if (target.addEventListener) {
+     		target.addEventListener(type, handler , false);
+     	} else if (target.attachEvent) {
+     		target.attachEvent("on" + type, handler);
+     	} else {
+     		target["on" + type] = handler;
+     	}
+     }
+     
+     // 记录输入XXX个字符
+     function recordCharacter() {
+     	 var math_length = 0;
+         math_length = getLenght(this.value);
+         
+           
+         if ($("search_text").value !== "") {
                $("length").innerHTML = "小仙女总共输入" + parseInt(math_length) + "个字符";
                $("length").style.display = "block";
            } else {
                $("length").style.display = "none";
-           }
-       })
-
-       /*清除内容*/
-       $("clearfix").addEventListener('click', function() {
-
-           $("search_text").value = "";
+           }	
+     }
+     
+     // 清除内容
+    function clearCharacter() {
+    	 $("search_text").value = "";
            $("length").innerHTML = "";
            $("tip_text").style.display = "block";
+    }
+    
+     // 表单获取焦点触发事件
+    function isFocus() {
+    	 $("tip_text").style.display = "none";
+    }
+    
+    // 清除文字
+    function clearText() {
+    	 search.innerHTML = "";
+         $("len").innerHTML = "";
+    }
 
-       }, false);
+    addListener($("search_text"), "keyup", recordCharacter);
+    addListener($("clearfix"), "click", clearCharacter);   
+    addListener($("search_text"), "focus", isFocus);
+    addListener($("clearfix"), 'dbclick', clearText);
 
-       /*表单获取焦点触发事件*/
-       $("search_text").addEventListener('focus', function() {
-           $("tip_text").style.display = "none";
-       })
-
-       /*双击清空右侧文字*/
-       $("clearfix").addEventListener('dblclick', function() {
-           search.innerHTML = "";
-           $("len").innerHTML = "";
-       }, false);
-   }
+     }
+   
    _main();
 
 
